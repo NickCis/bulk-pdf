4bf4f75d0012d43ddb793b874ea8eac18809ddbc
Nicolas Cisco <ncis20@gmail.com>
2025-08-08 03:35:15 -0300

    vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import workerUrl from "pdfjs-dist/build/pdf.worker?url";
 import { GlobalWorkerOptions } from "pdfjs-dist";
+import { Analytics } from "@vercel/analytics/react";
 
 import "./index.css";
 import App from "./App.tsx";
@@ -11,5 +12,6 @@ GlobalWorkerOptions.workerSrc = workerUrl;
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 );
